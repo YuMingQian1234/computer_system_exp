fcaf46ee7fe9fc06f45099814479467792d1fbd2
YuMingQian1234 <143723920+YuMingQian1234@users.noreply.github.com>
2024-12-20 11:13:05 +0800

    Add files via upload
    完成了MEM模块，首先是确定要写入寄存器的值，判断是否是lw,lb,lh,lhu指令再进行相应的运算之后再进入WB段

--- MEM.v
+++ MEM.v
@@ -7,6 +7,10 @@ module MEM(
 
     input wire [`EX_TO_MEM_WD-1:0] ex_to_mem_bus,
     input wire [31:0] data_sram_rdata,
+    
+    output wire [37:0] mem_to_id,
+    
+
 
     output wire [`MEM_TO_WB_WD-1:0] mem_to_wb_bus
 );
@@ -30,15 +34,15 @@ module MEM(
 
     wire [31:0] mem_pc;
     wire data_ram_en;
-    wire [3:0] data_ram_wen;
+    wire [3:0] data_ram_wen , data_ram_readen;
     wire sel_rf_res;
     wire rf_we;
     wire [4:0] rf_waddr;
     wire [31:0] rf_wdata;
     wire [31:0] ex_result;
-    wire [31:0] mem_result;
 
     assign {
+        data_ram_readen,//79:76
         mem_pc,         // 75:44
         data_ram_en,    // 43
         data_ram_wen,   // 42:39
@@ -49,16 +53,31 @@ module MEM(
     } =  ex_to_mem_bus_r;
 
 
-
-    assign rf_wdata = sel_rf_res ? mem_result : ex_result;
-
+    assign rf_wdata =     (data_ram_readen==4'b1111 && data_ram_en==1'b1) ? data_sram_rdata 
+                        : (data_ram_readen==4'b0001 && data_ram_en==1'b1 && ex_result[1:0]==2'b00) ?({{24{data_sram_rdata[7]}},data_sram_rdata[7:0]})
+                        : (data_ram_readen==4'b0001 && data_ram_en==1'b1 && ex_result[1:0]==2'b01) ?({{24{data_sram_rdata[15]}},data_sram_rdata[15:8]})
+                        : (data_ram_readen==4'b0001 && data_ram_en==1'b1 && ex_result[1:0]==2'b10) ?({{24{data_sram_rdata[23]}},data_sram_rdata[23:16]})
+                        : (data_ram_readen==4'b0001 && data_ram_en==1'b1 && ex_result[1:0]==2'b11) ?({{24{data_sram_rdata[31]}},data_sram_rdata[31:24]})
+                        : (data_ram_readen==4'b0010 && data_ram_en==1'b1 && ex_result[1:0]==2'b00) ?({24'b0,data_sram_rdata[7:0]})
+                        : (data_ram_readen==4'b0010 && data_ram_en==1'b1 && ex_result[1:0]==2'b01) ?({24'b0,data_sram_rdata[15:8]})
+                        : (data_ram_readen==4'b0010 && data_ram_en==1'b1 && ex_result[1:0]==2'b10) ?({24'b0,data_sram_rdata[23:16]})
+                        : (data_ram_readen==4'b0010 && data_ram_en==1'b1 && ex_result[1:0]==2'b11) ?({24'b0,data_sram_rdata[31:24]})
+                        : (data_ram_readen==4'b0011 && data_ram_en==1'b1 && ex_result[1:0]==2'b00) ?({{16{data_sram_rdata[15]}},data_sram_rdata[15:0]})
+                        : (data_ram_readen==4'b0011 && data_ram_en==1'b1 && ex_result[1:0]==2'b10) ?({{16{data_sram_rdata[31]}},data_sram_rdata[31:16]})
+                        : (data_ram_readen==4'b0100 && data_ram_en==1'b1 && ex_result[1:0]==2'b00) ?({16'b0,data_sram_rdata[15:0]})
+                        : (data_ram_readen==4'b0100 && data_ram_en==1'b1 && ex_result[1:0]==2'b10) ?({16'b0,data_sram_rdata[31:16]})
+                        : ex_result;
     assign mem_to_wb_bus = {
-        mem_pc,     // 69:38
+        mem_pc,     // 41:38
         rf_we,      // 37
         rf_waddr,   // 36:32
         rf_wdata    // 31:0
     };
-
+     assign  mem_to_id =
+    {   rf_we,      // 37
+        rf_waddr,   // 36:32
+        rf_wdata    // 31:0
+    };
 
 
 
